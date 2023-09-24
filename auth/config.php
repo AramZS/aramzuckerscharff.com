21ffa338b4220d808ab317bd40b546e421f16663
Aram Zucker-Scharff <azuckers@gmu.edu>
2023-09-24 15:24:10 -0600

    Name site init

---
+++ auth/config.php
@@ -0,0 +1,5 @@
+<?php
+define('APP_URL', 'https://aramzuckerscharff.com/auth/');
+define('APP_KEY', '2217c573a89fcb7e1ce1826bfc3014034f95b3aff3ca6752e0c69b3cc934b9b1');
+define('USER_HASH', '705f1f2bd5cc94e2d3561f50aba07edd');
+define('USER_URL', 'https://aramzuckerscharff.com');
