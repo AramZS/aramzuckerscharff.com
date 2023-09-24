db00a04aa4dcb67dee8118cfd1f66791a2321010
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2023-09-24 18:24:08 -0400

    Fix to https

--- media.php
+++ media.php
@@ -2,15 +2,15 @@
 
 	<h2>Media coverage of my work</h2>
 	
-	<p><a href="http://www.niemanlab.org/2015/02/clicks-likes-and-comments-a-hacker-looks-into-facebooks-news-feed/" target="_blank">Nieman Lab's coverage of my social media experiment with PressForward and Facebook.</a></p>
-	<p><a href="http://www.prweb.com/releases/2015/02/prweb12488878.htm" target="_blank">Folio's Notable Technologies in Audience Tracking Tool.</a></p>
+	<p><a href="https://www.niemanlab.org/2015/02/clicks-likes-and-comments-a-hacker-looks-into-facebooks-news-feed/" target="_blank">Nieman Lab's coverage of my social media experiment with PressForward and Facebook.</a></p>
+	<p><a href="https://www.prweb.com/releases/2015/02/prweb12488878.htm" target="_blank">Folio's Notable Technologies in Audience Tracking Tool.</a></p>
 	<p><a href="https://www.journalism.co.uk/news/why-cfo-built-an-open-source-tool-for-embedding-comments/s2/a556039/" target="_blank">Journalism.co.uk coverage of my work on the Response Stack social comment embedding plugin.</a></p>
-	<p><a href="http://www.foliomag.com/2013/aram-zucker-scharff/#.UlqlpzzrvtN.facebook" target="_blank">My entry in the 2014 FOLIO 15 under 30.</a></p>
-	<p><a href="http://aramzs.me/journuk" target="_blank">Journalism.co.uk coverage of my work on the PressForward WordPress plugin.</a></p>
-	<p><a href="http://dcmediamakers.wordpress.com/2009/10/22/dc-media-makers-at-npr-oct-28th-presentation-by-aram-zucker-scharff/" target="_blank">Post previewing my presentation to DC Media Makers at NPR.</a></p>
-	<p><a href="http://www.hillel.org/about/news/2008/nov/masonvotes_2008november04.htm" target="_blank">Hillel's coverage of Mason Votes.</a></p>
-	<p><a href="http://vvi.onstreammedia.com/cgi-bin/visearch?user=pbs-newshour&template=play220asf_noprefs_ws.html&query=mason+votes&squery=%2BClipID:5+%2BVideoAsset:pbsnh103008&inputField=undefined&ccstart=2326015&ccend=2679684&videoID=pbsnh103008" target="_blank">Coverage of the Mason Votes project by The News Hour with Jim Lehrer.</a></p>
-	<p><a href="javascript:;" onclick="window.open('http://chronotope.org/vid3.html','myWin','scrollbars=no,width=554,height=458');">Interview on Fox 5 DC about Mason Votes</a></p>
-	<p><a href="http://www.youtube.com/watch?v=hNKKDkdqauc" target="_blank">Interview at Social Media Club DC</a></p>
+	<p><a href="https://www.foliomag.com/2013/aram-zucker-scharff/#.UlqlpzzrvtN.facebook" target="_blank">My entry in the 2014 FOLIO 15 under 30.</a></p>
+	<p><a href="https://aramzs.me/journuk" target="_blank">Journalism.co.uk coverage of my work on the PressForward WordPress plugin.</a></p>
+	<p><a href="https://dcmediamakers.wordpress.com/2009/10/22/dc-media-makers-at-npr-oct-28th-presentation-by-aram-zucker-scharff/" target="_blank">Post previewing my presentation to DC Media Makers at NPR.</a></p>
+	<p><a href="https://www.hillel.org/about/news/2008/nov/masonvotes_2008november04.htm" target="_blank">Hillel's coverage of Mason Votes.</a></p>
+	<p><a href="https://vvi.onstreammedia.com/cgi-bin/visearch?user=pbs-newshour&template=play220asf_noprefs_ws.html&query=mason+votes&squery=%2BClipID:5+%2BVideoAsset:pbsnh103008&inputField=undefined&ccstart=2326015&ccend=2679684&videoID=pbsnh103008" target="_blank">Coverage of the Mason Votes project by The News Hour with Jim Lehrer.</a></p>
+	<p><a href="javascript:;" onclick="window.open('https://chronotope.org/vid3.html','myWin','scrollbars=no,width=554,height=458');">Interview on Fox 5 DC about Mason Votes</a></p>
+	<p><a href="https://www.youtube.com/watch?v=hNKKDkdqauc" target="_blank">Interview at Social Media Club DC</a></p>
 	
 <?php include 'footer.php'; ?>
